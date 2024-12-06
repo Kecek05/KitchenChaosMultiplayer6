434df119de355e83435bb411d79c47415abb0f67
João Felipe <jfk.abrao@hotmail.com>
2024-12-06 12:22:57 -0300

    v7

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -55,6 +55,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
     }
 
     private void Update() {
+        if (!IsOwner) return;
         HandleMovement();
         HandleInteractions();
     }
