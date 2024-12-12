e34061c51a4d9e52d0f341a0f3e11755a03d12fb
João Felipe <jfk.abrao@hotmail.com>
2024-12-12 12:05:55 -0300

    v44
    more lobby

--- Assets/Scripts/KitchenGameLobby.cs
+++ Assets/Scripts/KitchenGameLobby.cs
@@ -1,12 +1,15 @@
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.Lobbies;
 using UnityEngine;
+using Unity.Services.Lobbies.Models;
 
 public class KitchenGameLobby : MonoBehaviour
 {
 
     public static KitchenGameLobby Instance { get; private set; }
 
+    //private Lobby joinedLobby;
 
     private void Awake()
     {
@@ -20,11 +23,24 @@ public class KitchenGameLobby : MonoBehaviour
     {
         if(UnityServices.State != ServicesInitializationState.Initialized)
         {
-            await UnityServices.InitializeAsync();
+            InitializationOptions initializationOptions = new InitializationOptions();
+
+            initializationOptions.SetProfile(Random.Range(0, 10000).ToString());
+
+            await UnityServices.InitializeAsync(initializationOptions);
 
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
         }
 
     }
+
+    //public async void CreateLobby(string lobbyName, bool isPrivate)
+    //{
+    //    //joinedLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, isPrivate, KitchenGameMultiplayer.MAX_PLAYER_AMOUNT, new CreateLobbyOptions
+    //    //{
+    //    //    IsPrivate = isPrivate,
+    //    //});
+    //}
+
 }
